a7096010c583b10007c477db67fa78b280fd847c
Heidar Thordarson <kuxi@kuxi-macbookpro.roam.corp.google.com>
2016-10-21 19:06:46 +0200

    initial commit

---
+++ src/main/java/org/derp/jaxl/Jaxl.java
@@ -0,0 +1,7 @@
+package org.derp.jaxl;
+
+public final class Jaxl {
+    public static boolean method() {
+        return true;
+    }
+}
---
+++ src/test/java/org/derp/jaxl/JaxlTest.java
@@ -0,0 +1,10 @@
+package org.derp.jaxl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public final class JaxlTest {
+    @Test public void testSomeLibraryMethod() {
+        assertFalse("someLibraryMethod should return 'true'", Jaxl.method());
+    }
+}
